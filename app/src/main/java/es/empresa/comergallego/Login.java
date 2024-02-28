929ab51181bfd54356d9bda2212766f22f25bc87
Yago <yagodominguezestevez@gmail.com>
2024-02-28 21:27:26 +0100

    Version 0.1

---
+++ app/src/main/java/es/empresa/comergallego/Login.java
@@ -0,0 +1,54 @@
+package es.empresa.comergallego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Login extends AppCompatActivity {
+
+    protected TextInputEditText caja1;
+    protected TextInputEditText caja2;
+    protected ImageView ima1;
+    protected Button button1;
+    protected Button button2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+        caja1 = (TextInputEditText) findViewById(R.id.text1_login);
+        caja2 = (TextInputEditText) findViewById(R.id.edit2_login);
+        ima1= (ImageView) findViewById(R.id.ima1_login);
+        button1 = (Button) findViewById(R.id.button1_login);
+        button2 = (Button) findViewById(R.id.boton2_login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+        switch (item.getItemId()) {
+            case R.id.item_salir_login:
+               System.exit(0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/es/empresa/comergallego/StartActivity.java
@@ -0,0 +1,46 @@
+package es.empresa.comergallego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start);
+
+
+        //agregar animaciones
+
+        Animation ani1= AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation ani2= AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView label1 = findViewById(R.id.label1_start);
+        ImageView ima1 = findViewById(R.id.ima1_star);
+
+
+        label1.setAnimation(ani2);
+        ima1.setAnimation(ani1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+    }
+}
