65aa69c997f889b51f9e5f4c829ef962be5df668
appa95 <lorenzojonay@gmail.com>
2024-04-08 08:04:21 +0200

    GestionUsuarios

--- app/src/main/java/es/empresa/comergallego/GestorBBDDOperacionesUsuarios.java
+++ app/src/main/java/es/empresa/comergallego/GestorBBDDOperacionesUsuarios.java
@@ -38,6 +38,23 @@ public class GestorBBDDOperacionesUsuarios {
             }
         }
     }
-}
 
+    // Implementación del método actualizarNombre
+    public static void actualizarNombre(String idUsuario, String nuevoNombre, Statement s) throws SQLException {
+        String consulta = "UPDATE usuarios SET nombre = '" + nuevoNombre + "' WHERE id_Usuario = '" + idUsuario + "'";
+        insertarModificarEliminar(consulta, s);
+    }
+
+    // Implementación del método actualizarApellido
+    public static void actualizarApellido(String idUsuario, String nuevoApellido, Statement s) throws SQLException {
+        String consulta = "UPDATE usuarios SET apellido = '" + nuevoApellido + "' WHERE id_Usuario = '" + idUsuario + "'";
+        insertarModificarEliminar(consulta, s);
+    }
+
+    // Implementación del método actualizarCorreoElectronico
+    public static void actualizarCorreoElectronico(String idUsuario, String nuevoCorreo, Statement s) throws SQLException {
+        String consulta = "UPDATE usuarios SET correoElectronico = '" + nuevoCorreo + "' WHERE id_Usuario = '" + idUsuario + "'";
+        insertarModificarEliminar(consulta, s);
+    }
+}
 
